5d2a28a55f6721cac91df53a47ed29d4255e30b4
Casey Morrison <cmorrison@affirma.com>
2017-08-08 17:25:59 -0700

    remove object files that aren't used anymore and add comments to the model

--- js/app/model.js
+++ js/app/model.js
@@ -1,5 +1,16 @@
 var Model = Model || {};
 
+/**
+ * Case Constructor to create sample entry for the case timeline.
+ *
+ * @param caseId: String id of the current case.
+ * @param product: String title of the product this case is involved with.
+ * @param issueCategory: String category the issue falls into.
+ * @param issue: String summary of the issue
+ * @param state: String status of the issue: open, closed
+ * @param timeline: String: Timeline object that has all the entries tied to the case.
+ * @param status: String stating the status of the case.
+ */
 Model.Case = (function() {
     function Case(caseId, product, issueCategory, issue, state, timeline, status) {
         this.caseId = caseId;
@@ -14,6 +25,11 @@ Model.Case = (function() {
     return Case;
 })();
 
+/**
+ * CaseTimeline Constructor 
+ *
+ * @param entries: An Array of history entry objects
+ */
 Model.CaseTimeline = (function() {
     function CaseTimeline(entries) {
         this.numOfCases = entries ? entries.length : 0,
@@ -23,6 +39,20 @@ Model.CaseTimeline = (function() {
     return CaseTimeline;
 })();
 
+
+/**
+ * HistoryEntry Constructor to create sample entry for the case timeline.
+ *
+ * @param note: A string note for this entry.
+ * @param actions: An array of actions taken on this entry.
+ * @param id: The id string of the entry for scroll feature attached to the <li> element.
+ * @param date: The date string of the entry.
+ * @param name: String name of the (not sure who exactly).
+ * @param solution: String solution of the entry.
+ * @param slaExp: Boolean if the Sla is expired
+ * @param reOpen: Boolean if the case was reopened
+ * @param closed: Boolean if the case was closed
+ */
 Model.HistoryEntry = (function() {
     function HistoryEntry(note, actions, id, date, name, solution, slaExp, reOpen, closed) {
         this.note = note || "";
